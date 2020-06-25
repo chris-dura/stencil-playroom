f5844d44ccfc91cdd4c042f15b07b3469b93f14e
Christopher Dura <chris-dura@users.noreply.github.com>
2020-06-25 15:17:24 -0600

    chore: Add `@stencil/react-output-target` and build project

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # stencil-playroom
-Example repo integrating StencilJS and PlayroomJS
+
+Example repo integrating StencilJS and PlayroomJS.
+
+Based on https://github.com/ionic-team/stencil-ds-plugins-demo
--- my-library/stencil.config.ts
+++ my-library/stencil.config.ts
@@ -1,9 +1,14 @@
 import { Config } from '@stencil/core';
+import { reactOutputTarget } from '@stencil/react-output-target';
 
 export const config: Config = {
   namespace: 'my-library',
   taskQueue: 'async',
   outputTargets: [
+    reactOutputTarget({
+      componentCorePackage: 'my-library',
+      proxiesFile: '../my-library-react/src/components.ts'
+    }),
     {
       type: 'dist',
       esmLoaderPath: '../loader'
