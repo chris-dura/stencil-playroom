b7d6a1dcab334aafe34fa0a9fb2145d5a95ff24b
Christopher Dura <chris-dura@users.noreply.github.com>
2020-06-26 11:16:36 -0600

    chore: Build React package

---
+++ my-library-react/dist/components.d.ts
@@ -0,0 +1,3 @@
+/// <reference types="react" />
+import { JSX } from 'my-library';
+export declare const MyComponent: import("react").ForwardRefExoticComponent<JSX.MyComponent & import("react").HTMLAttributes<HTMLMyComponentElement> & import("./react-component-lib/utils").ReactProps & import("react").RefAttributes<HTMLMyComponentElement>>;
